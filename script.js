45c0abbfba42392b7d36707b3266fb2f5811618d
Malvina Hasa <hasamalvina24@gmail.com>
2022-09-30 01:22:20 -0400

    modified code to meet first Acceptance criteria regarding the promed question for the length of the password

--- script.js
+++ script.js
@@ -8,28 +8,27 @@ function generatePassword(){
   // whether they chose numbers, lowercase, uppercase or special character
 
   // promt questions 
-      // how many char? (between 8-128)
+      // how many char? (between 8-128) this is our length
+   do {
+    var userInput = Number(window.prompt("How many characters do you want to use between 8 and 128?", ""));
+   } while (userInput < 8 || userInput >128); 
       // lowercase? Y/N
       // uppercase? Y/N
       // special character? Y/N
 
+      // prompt("Do you want to use numbers?");
+      // prompt("Do you want to use lowercase characters?")
+      // prompt("Do you want ot use uppercase characters?")
+      // prompt("Do you want to use special characters?")
+
   // put selected character together
+  // start with an empty string and add data as you collect the answers. If string empty - ask again until we have at least one YES answer to the criteria questions.
+  var password = ""; 
 
   // rendomly select characters from pool
 
   // put them together
-
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 8;
-  var password = "";
   
-  const aNumber = Number(window.prompt("How many characters do you want to use between 8 and 128?", ""));
-  prompt("Do you want to use numbers?");
-  prompt("Do you want to use lowercase characters?")
-  prompt("Do you want ot use uppercase characters?")
-  prompt("Do you want to use special characters?")
-  
-  passwordLength = aNumber;
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -42,3 +41,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
