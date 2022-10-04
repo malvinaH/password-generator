9b9f0401e1e998e3d92a7219de3ac082ee1b460e
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-03 20:22:33 -0400

    created a series of prompts for password criteria

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
 // Assignment Code
+// function generatePassword(){
+  // Data 
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-  // Data
-  // we need to know the number of characters
-  // different sets of character : numbers, lowercase, uppercase, special char
-  // whether they chose numbers, lowercase, uppercase or special character
-
-  // promt questions 
-      // how many char? (between 8-128) this is our length
-   do {
-    var userInput = Number(window.prompt("How many characters do you want to use between 8 and 128?", ""));
-   } while (userInput < 8 || userInput >128); 
-      // lowercase? Y/N
-      // uppercase? Y/N
-      // special character? Y/N
-
-      // prompt("Do you want to use numbers?");
-      // prompt("Do you want to use lowercase characters?")
-      // prompt("Do you want ot use uppercase characters?")
-      // prompt("Do you want to use special characters?")
-
-  // put selected character together
-  // start with an empty string and add data as you collect the answers. If string empty - ask again until we have at least one YES answer to the criteria questions.
-  var password = ""; 
-
-  // rendomly select characters from pool
-
-  // put them together
-  
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  }
+function getPasswordLength(){
+  do {
+    var passwordLength = parseInt(prompt("How many characters do you want to use between 8 and 128"));
+  } while (passwordLength <8 || passwordLength > 128);
+  return passwordLength;
 }
+
+function generatePassword(){
+    // Data
+    var numbers = "1234567890";
+    var lowerCaseCharacters = "absdefghijklmnpqrstuvwxyz";
+    var upperCaseCharacters = lowerCaseCharacters.toUpperCase();
+    var specialCharacter = "!@#$%^&*()";
+    var choices = "";
+    var password = "";
+
+
+    var passwordLength = getPasswordLength();
+    console.log(passwordLength);
+
+    if (confirm("Would you like to use a number?")) choices += numbers;
+    if (confirm("Would you like to use a lowercase character?")) choices += lowerCaseCharacters;
+    if (confirm("Would you like to use an uppercase character?")) choices += upperCaseCharacters;
+    if (confirm("Would you like to use a special character?")) choices += specialCharacter;
+}
+
+    function writePassword(){
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      
+      passwordText.value = password;
+    }
+
+generatePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
