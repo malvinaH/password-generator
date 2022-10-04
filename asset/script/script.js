ddb995c94629c72a2a9d43c6431e8bdb205d2590
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-03 20:44:52 -0400

    placed files into new respective folders.

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,7 +1,7 @@
 
 var generateBtn = document.querySelector("#generate");
 
-// user input for password length
+// user input for password length.
 function getPasswordLength(){
   do {
     var passwordLength = parseInt(prompt("How many characters do you want to use between 8 and 128"));
@@ -10,6 +10,7 @@ function getPasswordLength(){
 }
 
 function generatePassword(){
+     // DATA - numbers, lowercase characters, uppercase characters, special characters.
     var numbers = "1234567890";
     var lowerCaseCharacters = "absdefghijklmnpqrstuvwxyz";
     var upperCaseCharacters = lowerCaseCharacters.toUpperCase();
@@ -20,11 +21,17 @@ function generatePassword(){
 
     var passwordLength = getPasswordLength();
 
+    // asking for character type to include in the password and put them together
     if (confirm("Would you like to use a number?")) choices += numbers;
     if (confirm("Would you like to use a lowercase character?")) choices += lowerCaseCharacters;
     if (confirm("Would you like to use an uppercase character?")) choices += upperCaseCharacters;
     if (confirm("Would you like to use a special character?")) choices += specialCharacter;
+    
+    if (!numbers && !lowerCaseCharacters && !upperCaseCharacters && !specialCharacter){
+      choices = alert("You must choose a criteria!");
+        }
 
+    // randomly select characters fromm all the selected choices and put them together in a string assigned to password var.
     for (var i=0 ; i < passwordLength; i++){
       var randomIndex = Math.floor(Math.random()* choices.length)
       var randomCharacter = choices[randomIndex];
